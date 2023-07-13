eb8e03d3ad2126c0662c6db1a3cb69e556191dd6
th3mon <p.szelenberger@gmail.com>
2023-07-13 05:47:37 +0200

    Configurate Jest

---
+++ js/jest.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  coverageProvider: "v8",
+
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+};
+
+export default config;
+
