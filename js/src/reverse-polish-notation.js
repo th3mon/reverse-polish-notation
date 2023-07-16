42781b1f468f0b4e9b7782f30f772ee40a4e0d1f
th3mon <p.szelenberger@gmail.com>
2023-07-16 08:02:39 +0200

    Make plugins Use Case run
    Extra small changes

--- js/src/reverse-polish-notation.js
+++ js/src/reverse-polish-notation.js
@@ -1,26 +1,25 @@
-export const reversePolishNotation = (expression) => {
+export const reversePolishNotation = (expression, options = {}) => {
   const operations = {
-    '+': (a, b) => Number(a) + Number(b),
-    '-': (a, b) => Number(a) - Number(b),
-    '*': (a, b) => Number(a) * Number(b),
-    '/': (a, b) => Number(a) / Number(b),
-    '^': (a, b) => Math.pow(Number(a), Number(b)),
-  }
-  const stack = []
-  const separator = ' '
+    "+": (a, b) => Number(a) + Number(b),
+    "-": (a, b) => Number(a) - Number(b),
+    "*": (a, b) => Number(a) * Number(b),
+    "/": (a, b) => Number(a) / Number(b),
+    "^": (a, b) => Math.pow(Number(a), Number(b)),
+    ...options?.operations,
+  };
+  const stack = [];
+  const separator = " ";
 
-  expression.split(separator)
-    .map(element => {
-      if (operations[element]) {
-        const result = operations[element](stack.pop(), stack.pop())
-
-        stack.push(result)
-      } else {
-        stack.push(element)
-      }
-    })
+  expression.split(separator).map((element) => {
+    if (operations[element]) {
+      const result = operations[element](stack.pop(), stack.pop());
+      stack.push(result);
+    } else {
+      stack.push(element);
+    }
+  });
 
   return stack.length > 1
     ? reversePolishNotation(stack.join(separator))
-    : stack[0]
-}
+    : stack[0];
+};
--- js/src/reverse-polish-notation.test.js
+++ js/src/reverse-polish-notation.test.js
@@ -1,43 +1,55 @@
-import { reversePolishNotation } from './reverse-polish-notation'
+import { reversePolishNotation } from "./reverse-polish-notation";
 
-describe('Reverse Polish Notation', () => {
-  it('Should exist', () => {
-    expect(reversePolishNotation).toBeDefined()
-  })
+describe("Reverse Polish Notation", () => {
+  it("Should exist", () => {
+    expect(reversePolishNotation).toBeDefined();
+  });
 
-  describe('simple use cases', () => {
-    it('Should add numbers', () => {
-      expect(reversePolishNotation('1 2 +')).toEqual(3)
-    })
+  describe("simple use cases", () => {
+    it("Should add numbers", () => {
+      expect(reversePolishNotation("1 2 +")).toEqual(3);
+    });
 
-    it('Should substract numbers', () => {
-      expect(reversePolishNotation('2 1 -')).toEqual(-1)
-    })
+    it("Should substract numbers", () => {
+      expect(reversePolishNotation("2 1 -")).toEqual(-1);
+    });
 
-    it('Should multiply numbers', () => {
-      expect(reversePolishNotation('2 3 *')).toEqual(6)
-    })
+    it("Should multiply numbers", () => {
+      expect(reversePolishNotation("2 3 *")).toEqual(6);
+    });
 
-    it('Should divide numbers', () => {
-      expect(reversePolishNotation('2 3 /')).toEqual(1.5)
-    })
+    it("Should divide numbers", () => {
+      expect(reversePolishNotation("2 3 /")).toEqual(1.5);
+    });
 
-    it('Should reverse polish notation', () => {
-      expect(reversePolishNotation('2 3 ^')).toEqual(9)
-    })
-  })
+    it("Should reverse polish notation", () => {
+      expect(reversePolishNotation("2 3 ^")).toEqual(9);
+    });
+  });
 
-  describe('combo use cases', () => {
-    it('Should add and substract numbers', () => {
-      expect(reversePolishNotation('7 1 1 + -')).toEqual(-5)
-    })
+  describe("combo use cases", () => {
+    it("Should add and substract numbers", () => {
+      expect(reversePolishNotation("7 1 1 + -")).toEqual(-5);
+    });
 
-    it('Should handle more complicated use case', () => {
-      expect(reversePolishNotation('15 7 1 1 + - / 3 * 2 1 1 + + -')).toEqual(5)
-    })
-  })
+    it("Should handle more complicated use case", () => {
+      expect(reversePolishNotation("15 7 1 1 + - / 3 * 2 1 1 + + -")).toEqual(
+        5
+      );
+    });
+  });
 
-  it.skip('Should execute modulo', () => {
-    expect(reversePolishNotation('2 3 %')).toEqual(0)
-  })
-})
+  describe("plugins", () => {
+    it("Should execute modulo", () => {
+      const modulo = (a, b) => Number(a) % Number(b);
+
+      expect(
+        reversePolishNotation("2 2 %", {
+          operations: {
+            "%": modulo,
+          },
+        })
+      ).toEqual(0);
+    });
+  });
+});
